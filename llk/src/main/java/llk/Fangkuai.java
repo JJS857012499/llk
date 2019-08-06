da59c0c73dda64df1f3053040ec48237fe19800e
江俊升 <857012499@qq.com>
2019-08-06 13:52:17 +0800

    feature:调整

---
+++ llk/src/main/java/llk/Fangkuai.java
@@ -0,0 +1,27 @@
+package llk;
+
+/**
+ * 方块数据
+ */
+public class Fangkuai {
+
+    private byte[] b;
+
+    public Fangkuai(byte[] b) {
+        this.b = b;
+    }
+
+    public byte[] getBytes() {
+        return b;
+    }
+
+    public boolean same(Fangkuai fk) {
+        byte[] a = fk.getBytes();
+        for (int i = 0; i < b.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
